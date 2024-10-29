607cfd12c659dd498dbb4aad15ec6936e1e33a11
shreeteja22 <smutukundu2206@gmail.com>
2024-10-29 20:10:52 +0530

    done

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 print("Welcome to My Project.\nTHE ULTIMATE QUIZ GAME")
 
-playing = input("Do you want to play the game? ")
+playing = input("Do you want to play the game? ").strip().lower()
 
 if playing.lower() != "yes":
     quit()
@@ -9,56 +9,56 @@ if playing.lower() != "yes":
 print("Let's Play! Quiz on Avengers!")
 score = 0
 
-answer = input("1.What is the real name of Thor ? ")
-if answer.lower() == 'chris hemsworth':
+answer = input("1. What is the real name of Thor? ").strip().lower()
+if answer == 'chris hemsworth':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("2.What is the name of iron man in movie ? ")
+answer = input("2.What is the name of iron man in movie ? ").strip().lower()
 if answer.lower() == 'tony stark':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("3.Aunt may is aunt of which avenger? ")
+answer = input("3.Aunt may is aunt of which avenger? ").strip().lower()
 if answer.lower() == 'spiderman':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("4.Steve rogers is referred as whom ? ")
+answer = input("4.Steve rogers is referred as whom ? ").strip().lower()
 if answer.lower() == 'captain america':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("5.Samuel Jackson played which role? ")
+answer = input("5.Samuel Jackson played which role? ").strip().lower()
 if answer.lower() == 'nick fury':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("6.What is the real name of Doctor strange? ")
+answer = input("6.What is the real name of Doctor strange? ").strip().lower()
 if answer.lower() == 'benedict cumberbatch':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("7.What is the name of thor's hammer ? ")
+answer = input("7.What is the name of thor's hammer ? ").strip().lower()
 if answer.lower() == 'mjolnir':
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("Director of Avengers Endgame ? ")
+answer = input("Director of Avengers Endgame ? ").strip().lower()
 if answer.lower() == 'Russo brothers':
     print("Correct!")
     score += 1
